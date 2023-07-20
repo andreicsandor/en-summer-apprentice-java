d1eb8299c573e7f2a8baccdd8e466937c887fd11
Andrei-Cristian S <andrei.sandor@outlook.com>
2023-07-20 18:22:25 +0300

    Move Mapper from Controller to Service class

--- src/main/java/com/system/ticketmanagement/service/EventService.java
+++ src/main/java/com/system/ticketmanagement/service/EventService.java
@@ -1,5 +1,7 @@
 package com.system.ticketmanagement.service;
 
+import com.system.ticketmanagement.dto.EventDTO;
+import com.system.ticketmanagement.mapper.EventMapper;
 import com.system.ticketmanagement.model.Event;
 import com.system.ticketmanagement.model.EventType;
 import com.system.ticketmanagement.model.Venue;
@@ -7,6 +9,7 @@ import com.system.ticketmanagement.repository.IEventDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -14,28 +17,53 @@ public class EventService implements IEventService {
 
     @Autowired
     private IEventDAO eventDAO;
+    @Autowired
+    private IVenueService venueService;
+    @Autowired
+    private IEventTypeService eventTypeService;
+    @Autowired
+    private EventMapper eventMapper;
 
     @Override
-    public List<Event> findEvents() {
+    public List<EventDTO> findEvents() {
         List<Event> events = eventDAO.findAll();
-        return events;
+        return eventMapper.convertDTOs(events);
     }
 
     @Override
-    public List<Event> findEventsByVenueAndType(Venue venue, EventType eventType) {
-        List<Event> events = eventDAO.searchByVenueAndType(venue, eventType);
-        return events;
+    public List<EventDTO> findEventsByVenueAndType(Long venueId, String eventTypeName) {
+        Venue venue = venueService.findVenueById(venueId);
+        EventType eventType = eventTypeService.findEventTypeByName(eventTypeName);
+
+        if (venue != null && eventType != null) {
+            List<Event> events = eventDAO.searchByVenueAndType(venue, eventType);
+            return eventMapper.convertDTOs(events);
+        } else {
+            return Collections.EMPTY_LIST;
+        }
     }
 
     @Override
-    public List<Event> findEventsByVenue(Venue venue) {
-        List<Event> events = eventDAO.searchByVenue(venue);
-        return events;
+    public List<EventDTO> findEventsByVenue(Long venueId) {
+        Venue venue = venueService.findVenueById(venueId);
+
+        if (venue != null) {
+            List<Event> events = eventDAO.searchByVenue(venue);
+            return eventMapper.convertDTOs(events);
+        } else {
+            return Collections.EMPTY_LIST;
+        }
     }
 
     @Override
-    public List<Event> findEventsByType(EventType eventType) {
-        List<Event> events = eventDAO.searchByType(eventType);
-        return events;
+    public List<EventDTO> findEventsByType(String eventTypeName) {
+        EventType eventType = eventTypeService.findEventTypeByName(eventTypeName);
+
+        if (eventType != null) {
+            List<Event> events = eventDAO.searchByType(eventType);
+            return eventMapper.convertDTOs(events);
+        }
+
+        return Collections.EMPTY_LIST;
     }
 }
--- src/main/java/com/system/ticketmanagement/service/IEventService.java
+++ src/main/java/com/system/ticketmanagement/service/IEventService.java
@@ -1,14 +1,12 @@
 package com.system.ticketmanagement.service;
 
-import com.system.ticketmanagement.model.Event;
-import com.system.ticketmanagement.model.EventType;
-import com.system.ticketmanagement.model.Venue;
+import com.system.ticketmanagement.dto.EventDTO;
 
 import java.util.List;
 
 public interface IEventService {
-    List<Event> findEvents();
-    List<Event> findEventsByVenueAndType(Venue venue, EventType eventType);
-    List<Event> findEventsByVenue(Venue venue);
-    List<Event> findEventsByType(EventType eventType);
+    List<EventDTO> findEvents();
+    List<EventDTO> findEventsByVenueAndType(Long venueId, String eventTypeName);
+    List<EventDTO> findEventsByVenue(Long venueId);
+    List<EventDTO> findEventsByType(String eventTypeName);
 }
