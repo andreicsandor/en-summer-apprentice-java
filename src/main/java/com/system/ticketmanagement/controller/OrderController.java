c89095cab85140dd9a7782e39adff2aa3fb1ac45
Andrei-Cristian S <andrei.sandor@outlook.com>
2023-07-21 09:13:03 +0300

    Tidy up code

--- src/main/java/com/system/ticketmanagement/controller/OrderController.java
+++ src/main/java/com/system/ticketmanagement/controller/OrderController.java
@@ -1,18 +1,12 @@
 package com.system.ticketmanagement.controller;
 
-import com.system.ticketmanagement.dto.EventDTO;
 import com.system.ticketmanagement.dto.OrderDTO;
-import com.system.ticketmanagement.service.IEventService;
-import com.system.ticketmanagement.service.IEventTypeService;
 import com.system.ticketmanagement.service.IOrderService;
-import com.system.ticketmanagement.service.IVenueService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.Collections;
 import java.util.List;
 
 @RestController
